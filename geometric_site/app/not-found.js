c2a56d8571d020a9741ef06a5233b8d191d932c7
maanemann <hans.august@gmail.com>
2023-08-11 14:45:28 +0200

    Add timer redirection fra 404 som ikke virker :(

--- geometric_site/app/not-found.js
+++ geometric_site/app/not-found.js
@@ -1,5 +1,21 @@
 
-export default function Home() {
+// Okay så redirection virker ikke (Error: NextRouter was not mounted) -- virker routing på en ny måde i next 13? Prøv at klikke på linket i fejlen..
+// Her er den tut, som jeg har fulgt: https://www.youtube.com/watch?v=O3yKwz4wRHc
+// Troels sagde at ifb. API får vi brug for "useParams" og "useState". Ved ikke om det er en hjælp, men skrev det ned..
+
+'use client';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000)
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Geometric Interactive</h1>
@@ -7,3 +23,5 @@ export default function Home() {
     </main>
   )
 }
+
+export default NotFound;
